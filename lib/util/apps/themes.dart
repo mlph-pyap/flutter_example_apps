aba6f1b01694d46f82c80456d3323362f5cb364e
mlph-pyap <mlph-pyap@mlph.168.1.13>
2020-12-09 18:38:37 +0800

    Fixing linter

--- lib/util/apps/themes.dart
+++ lib/util/apps/themes.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 ThemeData ThemeApps() {
   return ThemeData(
     primarySwatch: Colors.blue,
-    visualDensity: VisualDensity.adaptivePlatformDensity,
+    //visualDensity: VisualDensity.adaptivePlatformDensity,
   );
 }
 
