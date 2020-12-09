bdadf706f01d0d7bde2e195eaa442ffadd4cb6ea
mlph-pyap <mlph-pyap@mlph.168.1.13>
2020-12-09 18:09:43 +0800

    Adding test widget

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,12 +8,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-//import 'package:potatoes_app/main.dart';
+import 'package:potatoes_app/main.dart';
 //import 'package:potatoes_app/util/apps/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-   
+   	main();
   });
 }
